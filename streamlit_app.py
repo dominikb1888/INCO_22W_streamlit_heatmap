8c3c366ef86da2b0bea353eee89d59af54dbef4c
Dominik Boehler <dominik.boehler@gmx.net>
2022-11-05 14:40:32 +0100

    added function for displying the visual

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,31 +39,34 @@ pivot_table = pivot_table.sort_values('sum_cols', ascending=False).iloc[0:20]
 pivot_table.drop('sum_cols', axis=1, inplace=True)
 
 sns.set(rc={'figure.figsize':(11,44)})
-start_date = datetime.strptime("2022-03-16 11:00:00", '%Y-%m-%d %H:%M:%S')
 
-pivot_table = (dff[dff.updatedAt < start_date + timedelta(days = 230)]
+
+def heatmap_plot(date=datetime.strptime("2022-03-16 11:00:00", '%Y-%m-%d %H:%M:%S')):
+    pivot_table = (dff[dff.updatedAt < date)]
                    .pivot_table(index="user_login", columns="session_no", values='url', aggfunc='count'))
 
-heatmap_plot = sns.heatmap(
-    pivot_table,
-    annot_kws = {
-        'fontsize': 11,
-        'va':'center_baseline',
-    },
-    vmin = 1,
-    vmax = 17,
-    cmap = 'crest',
-    annot = True,
-    linecolor = 'white',
-    linewidths = .6,
-    cbar = False,
-    square = True,
-)
-heatmap_plot.set_xlabel('')
-heatmap_plot.set_ylabel('')
-heatmap_plot.set_yticklabels(heatmap_plot.get_yticklabels(), va='center_baseline')
-heatmap_plot.set_xticklabels(heatmap_plot.get_xticklabels(), ha='center')
-heatmap_plot.xaxis.tick_top()
+    heatmap_plot = sns.heatmap(
+        pivot_table,
+        annot_kws = {
+            'fontsize': 11,
+            'va':'center_baseline',
+        },
+        vmin = 1,
+        vmax = 17,
+        cmap = 'crest',
+        annot = True,
+        linecolor = 'white',
+        linewidths = .6,
+        cbar = False,
+        square = True,
+    )
+    heatmap_plot.set_xlabel('')
+    heatmap_plot.set_ylabel('')
+    heatmap_plot.set_yticklabels(heatmap_plot.get_yticklabels(), va='center_baseline')
+    heatmap_plot.set_xticklabels(heatmap_plot.get_xticklabels(), ha='center')
+    heatmap_plot.xaxis.tick_top()
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+date = st.sidebar.date_input('start date', datetime.date(2022,3,16))
+heatmap_plot(date)
 st.pyplot()
